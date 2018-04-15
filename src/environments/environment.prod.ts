c57ba067a98f875ebeef9c0f01f53d1cc0aaf0e3
aniruddh <mypositiveodisha@gmail.com>
2018-04-15 02:23:51 -0400

    GM-001
    - Add prod rest url to the config file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  restUrl: '',
+  restUrl: 'http://gauramarg-rest.appspot.com',
   trackAnalytics: true,
   useBasicAuth: false,
   alertDelayInSeconds: 7,
