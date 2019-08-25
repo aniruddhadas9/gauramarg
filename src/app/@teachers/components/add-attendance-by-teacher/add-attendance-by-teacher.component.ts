42f4c5274d019cff647afc7cb31ca36a18bb842e
Aniruddha Das <aniruddhadas9@gmail.com>
2019-08-25 02:03:21 -0700

    Add courses to gauramarg
     - Add attendance by teacher

--- src/app/@teachers/components/add-attendance-by-teacher/add-attendance-by-teacher.component.ts
+++ src/app/@teachers/components/add-attendance-by-teacher/add-attendance-by-teacher.component.ts
@@ -1,7 +1,7 @@
 import {Component, OnInit} from '@angular/core';
-import {Attendance, AttendanceService, Course, CourseRegistration, CourseRegistrationService} from '../../../@restapi';
+import {Attendance, AttendanceService, Course, CourseRegistration, CourseRegistrationService, User} from '../../../@restapi';
 import {UserService} from '@candiman/website';
-import {FormGroup} from '@angular/forms';
+import {FormArray, FormBuilder, FormControl, FormGroup, ValidatorFn, Validators} from '@angular/forms';
 import {FormlyFieldConfig} from '@ngx-formly/core';
 import {CoursesService} from '../../../@shared/services/courses/courses.service';
 import {forkJoin} from 'rxjs/internal/observable/forkJoin';
@@ -19,22 +19,31 @@ export class AddAttendanceByTeacherComponent implements OnInit {
   model = {};
   fields: FormlyFieldConfig[];
   courseRegistration: CourseRegistration;
+  courseOptions: Array<{ label: string, value: string }>;
   attendanceList = [];
+  attendanceForm: FormGroup;
 
   constructor(
     private userService: UserService,
     private coursesService: CoursesService,
     private courseRegistrationService: CourseRegistrationService,
-    private attendanceService: AttendanceService
+    private attendanceService: AttendanceService,
+    private formBuilder: FormBuilder
   ) {
+    this.attendanceForm = this.formBuilder.group({
+      courseId: new FormControl('', [Validators.required]),
+      classTime: new FormControl(''),
+      attendanceList: new FormArray([], this.minSelectedCheckboxes(1)),
+    });
+
   }
 
   ngOnInit() {
-    const courseOptions = this.coursesService.courses.map((couese: Course) => {
+    this.courseOptions = this.coursesService.courses.map((couese: Course) => {
       return {label: couese.name, value: couese.id};
     });
 
-    this.getAttendanceByCourseId(courseOptions[0].value as string);
+    // this.changeCourse(this.courseOptions[0].value as string);
 
     this.fields = [
       {
@@ -43,7 +52,7 @@ export class AddAttendanceByTeacherComponent implements OnInit {
         templateOptions: {
           label: 'Select a course',
           placeholder: '-- courses --',
-          options: courseOptions,
+          options: this.courseOptions,
           required: true,
           change: (field, $event) => {
             const courseId = $event.target.value.split(':')[1].trim();
@@ -58,34 +67,43 @@ export class AddAttendanceByTeacherComponent implements OnInit {
 
   }
 
-  getAttendanceByCourseId(courseID: string) {
-    forkJoin(
-      this.attendanceService.getByCourseIdUsingGET(courseID),
-      this.courseRegistrationService.getStudentsByCourseIdUsingGET(courseID),
-    )
-      .pipe(catchError(error => of(error)))
-      .subscribe((values) => {
-        if (values[1]) {
-          this.attendanceList = values[1] && values[1].map((value) => {
-            return {key: value.email, value: false};
-          });
-          this.fields.push({
-            key: 'attendance',
-            type: 'multicheckbox',
-            templateOptions: {
-              label: 'Attendance',
-              options: this.attendanceList
-            }
-          });
-        }
-        console.log(values);
-      });
-    /*this.attendanceService.getByCourseIdUsingGET(courseID).subscribe((attndence: Array<Attendance>) => {
-      console.log(attndence);
-      this.attendanceList[courseID] = attndence;
-    });*/
+  changeCourse(courseId: string) {
+    console.log(courseId);
+    this.courseRegistrationService.getStudentsByCourseIdUsingGET(courseId).subscribe((users: Array<User>) => {
+      console.log('students of the course %o is %o', courseId, users);
+      if (users) {
+        this.attendanceList = users && users.map((value) => {
+          // return {key: value.email, value: value.firstName + ' ' + value.lastName + '(' + value.email + ')'};
+          return {id: value.email, name: value.firstName + ' ' + value.lastName + '(' + value.email + ')'};
+        });
+        this.addCheckboxes(this.attendanceList);
+      }
+    });
   }
 
+  private addCheckboxes(students) {
+    console.log(students);
+    this.attendanceForm.controls.attendanceList = new FormArray([], this.minSelectedCheckboxes(1));
+    students.map((o, i) => {
+      const control = new FormControl(i === 0); // if first item set to true, else false
+      (this.attendanceForm.controls.attendanceList as FormArray).push(control);
+    });
+  }
+
+  minSelectedCheckboxes(min = 1) {
+    const validator: ValidatorFn = (formArray: FormArray) => {
+      const totalSelected = formArray.controls
+      // get a list of checkbox values (boolean)
+        .map(control => control.value)
+        // total up the number of checked checkboxes
+        .reduce((prev, next) => next ? prev + next : prev, 0);
+
+      // if the total is not greater than the minimum, return the error message
+      return totalSelected >= min ? null : {required: true};
+    };
+
+    return validator;
+  }
   submit(model) {
     console.log(model);
     if (this.form.valid) {
@@ -94,5 +112,54 @@ export class AddAttendanceByTeacherComponent implements OnInit {
       });
     }
   }
+
+  onSubmit() {
+    console.log(this.attendanceForm.getRawValue());
+    // Get all the selected values
+    const selectedOrderIds = this.attendanceForm.getRawValue().attendanceList
+      .map((value, index) => {
+        console.log('value, index', value, index);
+        return value ? this.attendanceList[index].id : null;
+      }).filter(value => value !== null);
+    console.log('selectedOrderIds:%o', selectedOrderIds);
+    selectedOrderIds.map((student) => {
+      const attendance: Attendance = {
+        approved: true,
+        courseId: '' + this.attendanceForm.getRawValue().courseId,
+        classTime: this.attendanceForm.getRawValue().classTime,
+        notes: 'Added by teacher',
+        studentId: student,
+        teacherId: this.userService.authorizedUser[0].email,
+      };
+
+      this.attendanceService.postUsingPOST(attendance).subscribe((response) => {
+        console.log('posted data to reset the form now', response);
+      });
+    });
+
+  }
+
+  resetForm() {
+    this.attendanceList = [];
+    this.attendanceForm.controls.attendanceList = new FormArray([], this.minSelectedCheckboxes(1));
+  }
+
+  getAttendanceByCourseId(courseID: string) {
+    forkJoin(
+      this.attendanceService.getByCourseIdUsingGET(courseID),
+      this.courseRegistrationService.getStudentsByCourseIdUsingGET(courseID),
+    )
+      .pipe(catchError(error => of(error)))
+      .subscribe((values) => {
+        if (values[1]) {
+          const students = values[1] && values[1].map((value) => {
+            // return {key: value.email, value: value.firstName + ' ' + value.lastName + '(' + value.email + ')'};
+            return {id: value.email, name: value.firstName + ' ' + value.lastName + '(' + value.email + ')'};
+          });
+          this.addCheckboxes(students);
+        }
+      });
+  }
+
 
 }
